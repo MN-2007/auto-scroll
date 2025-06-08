86c280d79698508a47c784620e3a39f3718fbef4
Mani Najdi <mani.najdi@gmail.com>
2025-06-08 13:03:52 +0330

    commit-01

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # auto-scroller 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver =webdriver.Chrome()
+driver.get("https://edition.cnn.com/")
+driver.maximize_window()
+
+scroll_pause_time = 1
+scroll_height = 1000
+
+driver.quit()
